4802c364d042e89aabc3768163634f28070eb311
hollierjm <hollierjm@gmail.com>
2022-02-20 01:38:44 -0800

    changes to event cards

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -4,6 +4,7 @@ import './profile.css'
 import { Card, Button } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Popup from 'reactjs-popup';
 
 const Profile = () => 
 {
@@ -28,21 +29,23 @@ const Profile = () =>
   */
 
   const cardInfo = [
-    { is_event: true, name: "Anthony", image: "", top_tags: "a, b, c", id: 1 },
-    { is_event: true, name: "Amy", image: "", top_tags: "a, b, c", id: 2 },
-    { is_event: true, name: "Casey", image: "", top_tags: "a, b, c", id: 3 }
+    { is_event: true, eventname: "Volleyball Open Gym", image: "", contact_info: "Varun Sreepathy", id: 1, src: "https://img.olympicchannel.com/images/image/private/t_16-9_360-203_2x/f_auto/v1536936974/primary/exvzqcvorticinejmmel" },
+    { is_event: true, eventname: "Hackathon", image: "", contact_info: "Danhiel Vu", id: 2, src: "https://news.microsoft.com/wp-content/uploads/prod/sites/45/2019/07/Teamshacking@Microsoft2019hackathon_-960x630.jpg" },
+    { is_event: true, eventname: "Soccery Intramural", image: "", contact_info: "Zackary Holly", id: 3, src: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_39/3509790/211001-nwsl-matches-al-1259.jpg" }
   ];
 
-  function RenderCard(props) {
+  function RenderEvents(props) {
     return (
       <Card style={{ width: '18rem' }} key={props.index}>
+        <Card.Img variant="top" src={props.src} />
         <Card.Title>{props.name}</Card.Title>
-        <Card.Body>{props.top_tags}</Card.Body>
+        <Card.Body>{props.contact_info}</Card.Body>
         <div className="button-wrapper">
-          <Button variant="primary">+</Button>
-        </div>
-        <div className="button-wrapper">
-          <Button variant="primary">see profile</Button>
+          <Popup trigger={<Button variant="primary" onClick={() => (console.log("hey i'm in hell"))}>Expand Event</Button>} modal>
+            <Card style={{ width: '30rem' }} >
+              <Card.Body>Hey I got it working!</Card.Body>
+            </Card>
+          </Popup>
         </div>
       </Card>
     );
@@ -62,7 +65,7 @@ const Profile = () =>
         <Row xs={1} md={3} className="g-4">
           {cardInfo.map(card =>
             <Col>
-              <RenderCard index={card.id} top_tags={card.top_tags} name={card.name}></RenderCard>
+              <RenderEvents index={card.id} top_tags={card.top_tags} name={card.name}></RenderEvents>
             </Col>
           )}
         </Row>
