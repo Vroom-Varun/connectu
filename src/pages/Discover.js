77c151fc36448019081134f95d178e4f78d2b40d
Katherine Pryal <katiepryal@live.com>
2022-02-19 18:05:09 -0800

    Added implementation for discover frinds page

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,3 +1,31 @@
-const Discover = () => <h1>Eat my ass world</h1>;
+import {Card, Button} from "react-bootstrap";
+import "./Discover.css"
 
+const Discover = () => {
+    const cardInfo = [
+        {is_event: true, name: "Anthony", image: "", top_tags: "a, b, c"},
+        {is_event: true, name: "Anthony", image: "", top_tags: "a, b, c"},
+        {is_event: true, name: "Anthony", image: "", top_tags: "a, b, c"}
+    ];
+
+    const renderCard = (card, index) => {
+        return (
+            <Card key={index}>
+                {/* <Card.Img>src={card.image}</Card.Img> */}
+                <Card.Title>{card.name}</Card.Title>
+                <Card.Body>{card.top_tags}</Card.Body>
+                <Button variant="primary">+</Button>
+                <Button variant="primary">see profile</Button>
+            </Card>
+        );
+    }
+    return(
+        <div className="Discover">  
+            <h1>AHHHHH</h1>
+            <div class="cards">
+                {cardInfo.map(renderCard)}
+            </div>
+        </div>
+    );
+}
 export default Discover;
