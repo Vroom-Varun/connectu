4bbeae7758021a3e3f459903c5d30a4d3d072841
Varurn Sreepathy <varun.sreepathy@gmail.com>
2022-02-19 20:20:37 -0800

    Fixed and cleaned up header and page

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,7 +1,8 @@
 import { Card, Button } from "react-bootstrap";
 import "./Discover.css"
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Discover = () => {
     const cardInfo = [
@@ -10,27 +11,33 @@ const Discover = () => {
         { is_event: true, name: "Casey", image: "", top_tags: "a, b, c", id: 3 }
     ];
 
-    function RenderCard (props){
+    function RenderCard(props) {
         return (
-            <Card  style={{ width: '18rem' }} key={props.index}>
-                {/* <Card.Img>src={card.image}</Card.Img> */}
+            <Card style={{ width: '18rem' }} key={props.index}>
                 <Card.Title>{props.name}</Card.Title>
                 <Card.Body>{props.top_tags}</Card.Body>
-                <Button variant="primary">+</Button>
-                <Button variant="primary">see profile</Button>
+                <div className="button-wrapper">
+                    <Button variant="primary">+</Button>
+                </div>
+                <div className="button-wrapper">
+                    <Button variant="primary">see profile</Button>
+                </div>
             </Card>
         );
     }
     return (
-        <div className="Discover">
-            <h1>AHHHHH</h1>
-            <div className="cards">
-                {/* {cardInfo.map(renderCard())} */}
-                {cardInfo.map(card =>
-                    <div className="card-position">
-                        <RenderCard index={card.id} top_tags={card.top_tags} name={card.name}></RenderCard>
-                    </div>
-                )}
+        <div>
+            <div className="title-wrapper">
+                <h1>Discover Connections</h1>
+            </div>
+            <div className="connections-box">
+                <Row xs={1} md={3} className="g-4">
+                    {cardInfo.map(card =>
+                        <Col>
+                                <RenderCard index={card.id} top_tags={card.top_tags} name={card.name}></RenderCard>
+                        </Col>
+                    )}
+                </Row>
             </div>
         </div>
     );
