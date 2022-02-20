78575509e5524407cbfbc58a509c02bcc6550615
hollierjm <hollierjm@gmail.com>
2022-02-20 00:38:22 -0800

    WIP: Redesign of profile user page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,8 @@
 import React, {useState} from 'react'
-import Box from '@material-ui/core/Box';
-import Card from 'react-bootstrap/Card';
-import Text from 'react-bootstrap/FormText'
-import Button from 'react-bootstrap/Button'
-import donutPic from './../images/donut.png'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './profile.css'
 import { ListGroup } from 'react-bootstrap';
+
 const Profile = () => 
 {
   const [imgPreview, setImgPreview] = useState(null);
@@ -27,72 +23,24 @@ const Profile = () =>
     }
   }
 
-  var profilePicture = donutPic;
-
   return(
-    <div style={{ marginLeft: '0%', marginTop: '0px', width: '100%', height: '60px' }}>
-    {/*
-    <Box color="white" bgcolor="palevioletred" p={1} height={200}>
-      {error && <p className="errorMsg">File not supported</p>}
-      <div className="imgPreview">
-        {!imgPreview&&(
-          <>
-          <p>Add and image</p>
-          <label htmlFor="fileUpload" className="customFileUpload">
-            Choose file
-            </label>
-            <input type="file" id="fileUpload" onChange={handleImageChange} />
-            <span>
-              (jpg, jpg or png)
-            </span>
-          </>
-          
-        )}
+    <body> 
+      <div id="PictureBox">
+        <img src="https://picsum.photos/id/237/200/300" alt="Profile Picture" id="ProfilePic"/>
       </div>
-    </Box>
-    */}
-      <Box color="white" bgcolor="palevioletred" p={1} height={50} fontSize={30}>
-        Profile page
-      </Box>
-      <div className="Pic">
-        <div className="ProfileBox">
-          <img src={profilePicture} height="120px" marginTop='20%'/>  
-        </div>
-      </div>
-      <div className='user-details'>
-        <p>Damien O'Neil</p>
-        <p>3rd Year (20)</p>
-        <p>Seattle University</p>
-      </div>
-      <div>
-      <div className='interests'>
-        <Card style={{width:'18rem'}}>
-          <Card.Header as="h5">Interests</Card.Header>
-          <ListGroup variant="flush">
-            <ListGroup.Item>Volleyball</ListGroup.Item>
-            <ListGroup.Item>Coding</ListGroup.Item>
-            <ListGroup.Item>Robotics</ListGroup.Item>
-          </ListGroup>
-        </Card>
-
-        <Card style={{width:'18rem'}}>
-          <Card.Header as="h5">Groups</Card.Header>
-          <ListGroup variant="flush">
-            <ListGroup.Item>Volleyball Club</ListGroup.Item>
-            <ListGroup.Item>SU ACM</ListGroup.Item>
-            <ListGroup.Item>SU Robotics</ListGroup.Item>
-          </ListGroup>
-        </Card>
-
-      </div>
-      </div>
-      
-
-      
-      
-    </div>
-    
+      <article id="BioBox">
+        <h1>Bill Billington Billards</h1>
+        <text>"Hello, my name is Bill and I love some thicc peaches. They are my favourite fruits."</text>
+      </article>
+      <h2>Bill's Recent Events</h2>
+      <section id="EventBox">
+        <div class="event"><img src="https://picsum.photos/seed/picsum/200/300" alt="Event 1" class="EventPic"/></div>
+        <div class="event"><img src="https://picsum.photos/seed/picsum/200/300" alt="Event 2" class="EventPic"/></div>
+        <div class="event"><img src="https://picsum.photos/seed/picsum/200/300" alt="Event 3" class="EventPic"/></div>
+        <div class="event"><img src="https://picsum.photos/seed/picsum/200/300" alt="Event 4" class="EventPic"/></div>
+      </section>
+    </body>
   );
 }
-export default Profile;
 
+export default Profile;
